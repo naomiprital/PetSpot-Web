71533554c7e7ccd588d9c46c141e76e40c1c592a
Naomi Prital <neomi.prital@gmail.com>
2026-01-10 18:40:42 +0200

    Change innitial project to typescript

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,13 @@
-const express = require('express');
+import { Request, Response } from "express";
+import express from 'express';
+import dotenv from 'dotenv';
+
 const app = express();
-const dotenv= require('dotenv');
 dotenv.config({ path: ".env.dev" });
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
 
