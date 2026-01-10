4a07a4f8d506a4fe11378b6ba26ad516c098163a
Naomi Prital <neomi.prital@gmail.com>
2026-01-10 18:56:30 +0200

    Add prettier to project

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,9 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 import express from 'express';
 import dotenv from 'dotenv';
 
 const app = express();
-dotenv.config({ path: ".env.dev" });
+dotenv.config({ path: '.env.dev' });
 
 app.use(express.json());
 
