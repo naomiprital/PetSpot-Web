b04dd022d6b4e7dc14a471322bf6f2b197196f80
Naomi Prital <neomi.prital@gmail.com>
2026-01-10 22:34:08 +0200

    Config typescript and add db connection

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,20 @@
 import { Request, Response } from 'express';
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './src/config/connect';
+
+dotenv.config({ path: '.env.dev' });
 
 const app = express();
-dotenv.config({ path: '.env.dev' });
+const port = process.env.PORT || 8080;
+
+connectDB();
 
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on http://localhost:${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
