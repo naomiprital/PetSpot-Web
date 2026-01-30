75bd89962a3f8a39921a293c0979d15bf14865dd
Naomi Prital <neomi.prital@gmail.com>
2026-01-30 19:06:35 +0200

    Fix photos field in posts type

--- server/src/types/post.ts
+++ server/src/types/post.ts
@@ -1,25 +1,33 @@
-import { ObjectId } from "mongoose";
+import { ObjectId } from 'mongoose';
 
-export const ANIMAL_TYPES = ['dog', 'cat', 'bird', 'rabbit', 'hamster', 'horse', 'other'] as const;
+export const ANIMAL_TYPES = [
+  'dog',
+  'cat',
+  'bird',
+  'rabbit',
+  'hamster',
+  'horse',
+  'other',
+] as const;
 export const POST_TYPES = ['lost', 'found'] as const;
 
-export type AnimalType = typeof ANIMAL_TYPES[number];
-export type PostType = typeof POST_TYPES[number];
+export type AnimalType = (typeof ANIMAL_TYPES)[number];
+export type PostType = (typeof POST_TYPES)[number];
 
 export interface Post {
-    _id?: ObjectId | string;
-    sender: string;
-    type: PostType;
-    animalType: AnimalType;
-    location: {
-        type: 'Point';
-        coordinates: number[];
-        address?: string;
-    };
-    dateTimeOccured: Date;
-    description?: string;
-    photos?: string[];
-    isResolved: boolean;
-    createdAt?: Date;
-    updatedAt?: Date;
+  _id?: ObjectId | string;
+  sender: string;
+  type: PostType;
+  animalType: AnimalType;
+  location: {
+    type: 'Point';
+    coordinates: number[];
+    address?: string;
+  };
+  dateTimeOccured: Date;
+  description?: string;
+  photos: string[];
+  isResolved: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
