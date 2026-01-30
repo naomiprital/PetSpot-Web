5e7276cd13d335899eeee6544e76af5b5e974090
Naomi Prital <neomi.prital@gmail.com>
2026-01-30 19:19:34 +0200

    Add back the question mark to the photos field in Post

--- server/src/types/post.ts
+++ server/src/types/post.ts
@@ -26,7 +26,7 @@ export interface Post {
   };
   dateTimeOccured: Date;
   description?: string;
-  photos: string[];
+  photos?: string[];
   isResolved: boolean;
   createdAt?: Date;
   updatedAt?: Date;
