7825dfadaf548b784101fdba3c762adc3e8ab18b
YuvalPechter <yuval30.07.2003@gmail.com>
2026-01-30 15:11:37 +0200

    update post with put method as required

--- server/src/routes/postsRouter.ts
+++ server/src/routes/postsRouter.ts
@@ -6,6 +6,6 @@ const postsRouter = Router();
 postsRouter.get('/', postsController.getPosts);
 postsRouter.post('/', postsController.createPost);
 postsRouter.get('/:id', postsController.getPostById);
-postsRouter.patch('/:id', postsController.updatePost);
+postsRouter.put('/:id', postsController.updatePost);
 
 export default postsRouter;
