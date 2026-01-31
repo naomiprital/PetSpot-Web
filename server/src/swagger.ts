ab205b450554607009ab9ee691d0ed532a1b131b
YuvalpPechter <yuval30.07.2003@gmail.com>
2026-01-31 23:22:00 +0200

    Change swagger port

--- server/src/swagger.ts
+++ server/src/swagger.ts
@@ -17,7 +17,7 @@ const options: swaggerJsdoc.Options = {
     },
     servers: [
       {
-        url: process.env.BASE_URL || 'http://localhost:3000',
+        url: process.env.BASE_URL || 'http://localhost:8080',
         description: 'Development server',
       },
     ],
