81c20b4d65e09191064e399c4fe3a2110e271569
YuvalPechter <yuval30.07.2003@gmail.com>
2026-04-23 17:43:27 +0300

    fix: users requests except the google login request

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
