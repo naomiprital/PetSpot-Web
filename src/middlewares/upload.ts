7dd6da197bb94cfdd905fdc0a34bd695400cfd40
Naomi Prital <neomi.prital@gmail.com>
2026-04-23 13:20:56 +0300

    rename ext to extention

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -13,8 +13,8 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    const ext = path.extname(file.originalname);
-    cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+    const extention = path.extname(file.originalname);
+    cb(null, `${file.fieldname}-${uniqueSuffix}${extention}`);
   },
 });
 
